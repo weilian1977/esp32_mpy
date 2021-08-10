09e7a36ebecd60179b8ad10efffdd20de12ef8f1
Yanminge <guojia198769@163.com>
2021-08-10 19:14:47 +0800

    test1

--- example/cdc_msc/main/main.c
+++ example/cdc_msc/main/main.c
@@ -27,9 +27,10 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "bsp/board.h"
+//#include "tusb.h"
+//#include "bsp/board.h"
 #include "tusb.h"
-#include "FreeRTOS.h"
+#include "freertos/FreeRTOS.h"
 #include "esp_task.h"
 
 //--------------------------------------------------------------------+
@@ -37,27 +38,24 @@
 //--------------------------------------------------------------------+
 #define SUB_TASK_PRIORITY        (ESP_TASK_PRIO_MIN + 1)
 #define SUB_TASK_STACK_SIZE      (16 * 1024)
-/* Blink pattern
- * - 250 ms  : device not mounted
- * - 1000 ms : device mounted
- * - 2500 ms : device is suspended
- */
-enum  {
-  BLINK_NOT_MOUNTED = 250,
-  BLINK_MOUNTED = 1000,
-  BLINK_SUSPENDED = 2500,
-};
 
-static uint32_t blink_interval_ms = BLINK_NOT_MOUNTED;
-
-void led_blinking_task(void);
 void cdc_task(void);
 void sub_task(void *pvParameter);
+
+#if CONFIG_IDF_TARGET_ESP32S3
+static void usb_otg_router_to_internal_phy()
+{
+  uint32_t *usb_phy_sel_reg = (uint32_t *)(0x60008000 + 0x120);
+  *usb_phy_sel_reg |= BIT(19) | BIT(20);
+}
+#endif
 
 /*------------- MAIN -------------*/
 int main(void)
 {
-  board_init();
+#if CONFIG_IDF_TARGET_ESP32S3
+  usb_otg_router_to_internal_phy();
+#endif
   tusb_init();
   printf("tusb_init\n");
   xTaskCreatePinnedToCore(sub_task, "sub_task", SUB_TASK_STACK_SIZE / sizeof(StackType_t), NULL, SUB_TASK_PRIORITY, NULL, 0);
@@ -69,53 +67,18 @@ int main(void)
   return 0;
 }
 
-#if CFG_TUSB_MCU == OPT_MCU_ESP32S2 || CFG_TUSB_MCU == OPT_MCU_ESP32S3
 void app_main(void)
 {
   main();
 }
-#endif
 
 void sub_task(void *pvParameter)
 {
     while(1)
     {
-      led_blinking_task();
       cdc_task();
     }
 }
-
-//--------------------------------------------------------------------+
-// Device callbacks
-//--------------------------------------------------------------------+
-
-// Invoked when device is mounted
-void tud_mount_cb(void)
-{
-  blink_interval_ms = BLINK_MOUNTED;
-}
-
-// Invoked when device is unmounted
-void tud_umount_cb(void)
-{
-  blink_interval_ms = BLINK_NOT_MOUNTED;
-}
-
-// Invoked when usb bus is suspended
-// remote_wakeup_en : if host allow us  to perform remote wakeup
-// Within 7ms, device must draw an average of current less than 2.5 mA from bus
-void tud_suspend_cb(bool remote_wakeup_en)
-{
-  (void) remote_wakeup_en;
-  blink_interval_ms = BLINK_SUSPENDED;
-}
-
-// Invoked when usb bus is resumed
-void tud_resume_cb(void)
-{
-  blink_interval_ms = BLINK_MOUNTED;
-}
-
 
 //--------------------------------------------------------------------+
 // USB CDC
@@ -165,18 +128,3 @@ void tud_cdc_rx_cb(uint8_t itf)
   (void) itf;
 }
 
-//--------------------------------------------------------------------+
-// BLINKING TASK
-//--------------------------------------------------------------------+
-void led_blinking_task(void)
-{
-  static uint32_t start_ms = 0;
-  static bool led_state = false;
-
-  // Blink every interval ms
-  if ( board_millis() - start_ms < blink_interval_ms) return; // not enough time
-  start_ms += blink_interval_ms;
-
-  board_led_write(led_state);
-  led_state = 1 - led_state; // toggle
-}
