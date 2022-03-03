d91fc9cdbf5047d35501d6ee430aec8144f06044
duzhipeng <duzhipeng@matatalab.com>
2022-03-03 15:31:04 +0800

    add oak_lite

---
+++ project/oak_lite/boards/OAK_LITE_V1/mpconfigboard.cmake
@@ -0,0 +1,12 @@
+set(IDF_TARGET esp32s3)
+
+set(SDKCONFIG_DEFAULTS
+    boards/sdkconfig.base
+    boards/OAK_LITE_V1/sdkconfig.usb
+    boards/OAK_LITE_V1/sdkconfig.spiram_sx
+    boards/OAK_LITE_V1/sdkconfig.board
+)
+
+if(NOT MICROPY_FROZEN_MANIFEST)
+    set(MICROPY_FROZEN_MANIFEST ${MICROPY_PORT_DIR}/boards/manifest.py)
+endif()
