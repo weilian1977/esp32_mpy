1da3357ed4d15d3122393224dc397ecfce4fb124
Yanminge <guojia198769@163.com>
2022-06-21 18:14:34 +0800

    fix motion scratch api issue

--- project/matatacar_v3/python_apps/drv_motion.py
+++ project/matatacar_v3/python_apps/drv_motion.py
@@ -4,7 +4,7 @@
 
 import matatalab
 import nvs
-
+import time
 import drv_system
 
 moto = matatalab.motion()
@@ -30,17 +30,15 @@ def turn_right(angle, wait_flag):
 def move_speed(speed_left, speed_right):
     moto.move_speed(speed_left, speed_right)
 
-def motor_pwm(speed_left, speed_right):
-    moto.motor_pwm(speed_left, speed_right)
-
 def move_position(dir, position, unit, wait_flag = False):
+    moto.set_max_speed(700 * 32)
     position_value = position
     if dir == "backward":
         position_value = -position_value
     if unit == "seconds":
         moto.move_speed(108, 108)
         time.sleep(abs(position_value))
-    elif unit == "mm":    
+    elif unit == "mm":
         position_value = int(position_value)
         moto.move_position(position_value, wait_flag)
     elif unit == "cm":    
@@ -51,34 +49,66 @@ def move_position(dir, position, unit, wait_flag = False):
         moto.move_position(position_value, wait_flag)
 
 def move_angle(dir, angle, unit, wait_flag = False):
+    moto.set_max_speed(700 * 32)
     angle_value = angle
     if dir == "left":
         angle_value = -angle_value
     if unit == "seconds":
         moto.move_speed(-108, 108)
         time.sleep(abs(angle_value))
-    elif unit == "rotations":    
-        angle_value = int(angle_value * 360.0)
-        angle = int(round(angle * (nvs.save_angle) / 90.0))
-        moto.move_angle(angle, wait_flag)
+    elif unit == "rotations":
+        angle_value = int(round(angle_value * 360 * (nvs.save_angle) / 90.0))
+        moto.move_angle(angle_value, wait_flag)
     elif unit == "degrees":    
-        angle_value = int(angle_value)
-        angle = int(round(angle * (nvs.save_angle) / 90.0))
-        moto.move_angle(angle, wait_flag)
+        angle_value = int(round(angle_value * (nvs.save_angle) / 90.0))
+        moto.move_angle(angle_value, wait_flag)
 
 def start_moving(dir):
     if dir == "forward":
         moto.move_speed(108, 108)
-    elif unit == "backward":
+    elif dir == "backward":
         moto.move_speed(-108, -108)
     elif dir == "left":
         moto.move_speed(-108, 108)
-    elif dir == "left":
+    elif dir == "right":
         moto.move_speed(108, -108)
 
 def stop_moving():
     return moto.stop(2)
 
+def motor_run(motor, value, unit, speed, dir, wait_flag = False):
+    motor_temp = motor
+    speed_temp = abs(int(speed))
+    if motor_temp == "A+B":
+        motor_temp = 2
+    elif motor_temp == "A":
+        motor_temp = 0
+    elif motor_temp == "B":
+        motor_temp = 1    
+    if unit == "seconds":
+        if(dir == "clockwise"):
+            moto.motor_speed(motor_temp, speed_temp)
+        else:
+            moto.motor_speed(motor_temp, speed_temp)
+            time.sleep(abs(value))
+    elif unit == "rotations":
+        moto.set_max_speed(speed_temp * 8 * 32)
+        pos = int(round(45.6 * 3.14 * value))
+        moto.motor_run(motor_temp, pos, wait_flag)    # WHEEL_DIAMETER_VALUE
+    elif unit == "degrees":
+        moto.set_max_speed(speed_temp * 8 * 32)
+        pos = int(round(45.6 * 3.14 * value / 360.0))
+        moto.motor_run(motor_temp, pos, wait_flag)    # WHEEL_DIAMETER_VALUE
+
+def motor_pwm(motor, speed):
+    motor_temp = motor
+    if motor_temp == "A+B":
+        motor_temp = 2
+    elif motor_temp == "A":
+        motor_temp = 0
+    elif motor_temp == "B":
+        motor_temp = 1
+    moto.motor_pwm(motor_temp, speed)
 
 def get_motion_status(motor):
     return moto.get_motion_status(motor)
@@ -86,12 +116,12 @@ def get_motion_status(motor):
 def get_motor_speed(motor):
     return moto.get_motor_speed(motor)
     
-def stop(mode):
-    mode_temp = mode
-    if mode == "A+B":
-        mode_temp = 2
-    elif mode == "A":
-        mode_temp = 0
-    elif mode == "B":
-        mode_temp = 1
-    moto.stop(mode)
+def stop(motor):
+    motor_temp = motor
+    if motor_temp == "A+B":
+        motor_temp = 2
+    elif motor_temp == "A":
+        motor_temp = 0
+    elif motor_temp == "B":
+        motor_temp = 1
+    moto.stop(motor_temp)
